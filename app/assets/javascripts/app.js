5c4d7e8418fff3835e499baeb5d90100c0d8b5b0
Thanh Huynh <thanhjamin@yahoo.com>
2014-02-01 21:21:48 -0800

    cleaned up ember files

--- app/assets/javascripts/app.js
+++
@@ -1,9 +0,0 @@
-App = Ember.Application.create({
-  LOG_TRANSITIONS: true
-});
-
-App.Router.map(function() {
-  this.resource('posts');
-  this.resource('post', { path: 'posts/:post_id' }, function() {
-  });
-});
